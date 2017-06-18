dab4c0a6376b7cc7bc4dca57ed92adbc7085f9e9
Nuno Araujo <nunobaraujo@hotmail.com>
2017-06-18 02:16:17 +0100

    Added demo apllications for server and client

---
+++ src/NBsoft.Sockets.Demo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBsoft.Sockets.DemoServer
+{
+    class Program
+    {
+        static SocketServer server;
+        const int port = 5550;
+        static void Main(string[] args)
+        {
+            server = new SocketServer(new System.Net.IPEndPoint(System.Net.IPAddress.Any, port));
+            server.ClientConnected += Server_ClientConnected;
+            server.ClientDisconnected += Server_ClientDisconnected;
+            server.ClientLog += Server_ClientLog;
+            server.ClientDataReceived += Server_ClientDataReceived;
+            server.ClientError += Server_ClientError;
+            server.Listen();
+            Console.WriteLine("Server started. Waiting connections on port {0}", port);
+            Console.ReadLine();
+        }
+
+        private static void Server_ClientError(object sender, ErrorEventArgs e)
+        {
+            Log("ERROR", e.Exception.Message);
+        }
+
+        private static void Server_ClientDataReceived(object sender, ClientEventArgs e)
+        {
+            string msg = Encoding.Unicode.GetString(e.Data);
+            string broadcastMsg = $"{e.Connection.Tag} > {msg}";
+            Log("MESSAGE", broadcastMsg);
+
+            // Forward message to all clients
+            server.SendAll(Encoding.Unicode.GetBytes(broadcastMsg));
+        }
+
+        private static void Server_ClientLog(object sender, LogEventArgs e)
+        {
+            Log("INFO", e.LogText);
+        }
+
+        private static void Server_ClientDisconnected(object sender, ClientEventArgs e)
+        {
+            string msg = $"Client Disconnected: {e.Connection.Tag}.\n\rClientList:";
+            foreach (var item in server.Clients)
+            {
+                msg += $"\n\r\t> {item.RemoteEndPoint} ({item.Tag})";
+            }
+            Log("INFO", msg);
+            server.SendAll(Encoding.Unicode.GetBytes($"Client Left: {e.Connection.Tag}"));
+        }
+
+        private static void Server_ClientConnected(object sender, ClientEventArgs e)
+        {
+            string msg = $"Client Connected: {e.Connection.RemoteEndPoint}.\n\rClientList:";
+            foreach (var item in server.Clients)
+            {
+                msg += $"\n\r\t> {item.RemoteEndPoint} ({item.Tag})";
+            }
+            Log("INFO", msg);
+            server.SendAll(Encoding.Unicode.GetBytes($"Client Joined: {e.Connection.Tag}"));
+        }
+
+        private static void Log(string type, string text)
+        {
+            Console.WriteLine("{0}|{1}|{2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), type, text);
+        }
+    }
+}
---
+++ src/NBsoft.Sockets.DemoClient/SocketsClientForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NBsoft.Sockets.DemoClient
+{
+    public partial class SocketsClientForm : Form
+    {
+        SocketClient client;
+        // buffer manager
+        internal static System.ServiceModel.Channels.BufferManager Buff = System.ServiceModel.Channels.BufferManager.CreateBufferManager(4096, 4096 * 3);
+        public SocketsClientForm()
+        {
+            InitializeComponent();
+            this.FormClosed += SocketsClientForm_FormClosed;
+            client = new SocketClient(Buff);
+            client.Connected += Client_Connected;
+            client.Disconnected += Client_Disconnected;
+            client.Error += Client_Error;
+            client.DataReceived += Client_DataReceived;
+        }
+
+        private void SocketsClientForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            client.Disconnect();
+            client.Dispose();
+        }
+
+        private void Client_DataReceived(object sender, SockMessageEventArgs e)
+        {
+            //Addind data to visual control must be run in form thread.
+            // Invoke it
+            this.Invoke((MethodInvoker)(() =>
+            {
+                string msg = Encoding.Unicode.GetString(e.SockMessage);
+                AddLine(msg);
+            }));
+;        }
+
+        private void Client_Error(object sender, ErrorEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
+
+        private void Client_Disconnected(object sender, EventArgs e)
+        {
+            AddLine("Client Disconected");
+        }
+
+        private void Client_Connected(object sender, EventArgs e)
+        {
+            AddLine("Client Connected");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!client.IsConnected)
+            {
+                //Connect to localhost on port 5550(demo server)
+                client.Connect(IPAddress.Parse("127.0.0.1"), 5550);
+                button1.Text = "Disconnect";
+                label1.Text = $"connected to {client.RemoteEndPoint}";
+            }
+            else
+            {
+                client.Disconnect();
+                button1.Text = "Connect";
+                label1.Text = "disconnected";
+            }
+        }
+        private void AddLine(string line)
+        {
+            listBox1.Items.Add(line);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendText();
+        }
+
+        private void SendText()
+        {
+            if (textBox1.Text.Length > 0)
+                client.Send(Encoding.Unicode.GetBytes(textBox1.Text));
+            textBox1.Text = "";
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendText();
+        }
+    }
+}
